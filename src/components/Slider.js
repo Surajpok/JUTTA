a769d2d86dea396f5d66572e51848e69049385df
surajpok <Pokhrel004@gmail.com>
2021-10-31 23:21:58 +0545

    commit

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,40 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import React from "react";
+import styled from "styled-components";
 
 const Slider = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
 };
 
 export default Slider;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+const Arrow = styled.div`
+  display: flex;
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+  postion: absolute;
+  top: 0;
+  buttom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+`;
